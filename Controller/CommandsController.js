21c16b9df40d609d880eb0cfd35104486cf6d51d
Thomas.L <thomasleconte05@gmail.com>
2020-12-03 08:51:42 +0100

    Start of controller implementation

--- Controller/CommandsController.js
+++ Controller/CommandsController.js
@@ -1,7 +1,46 @@
 const Discord = require("discord.js");
+const EmbedMessage = require("../View/EmbedMessage");
+const commandPrefix = "/";
 
 class CommandsController{
+    constructor(client){
+        this.client = client;
+    }
 
+    sendEmbededMessage(title, description){
+        let message = new EmbedMessage(
+            this.client, title, description);
+        return message.example;
+    }
+
+    analyseCommand(event){
+        //verifie que celui qui envoie le message est un bot
+        if(event.author.bot) return;
+        //si le message commence par quelque chose de different que le prefixe
+        if(!event.content.startsWith(commandPrefix)) return;
+    
+        //on supprime le prefixe
+        let commandBody = event.content.slice(commandPrefix.length);
+        //on divise le message en tableau
+        let args = commandBody.split(" ");
+        //on supprime le premier element du tableau qu'on retourne dans command
+        //et on le converti en lettre minuscule
+        let command = args.shift().toLowerCase();
+        
+        switch(command){
+            case "lmt":
+                event.channel.send(
+                    this.sendEmbededMessage(
+                    "Wtf is this bot ?",
+                    "LearnMoreTech is a bot for being aware of the latest news in the field of tech and IT development !")
+                );
+                break;
+        }
+    }
+
+    listen(){
+        this.client.on('message', event=> this.analyseCommand(event));
+    }
 }
 
 module.exports = CommandsController;
--- index.js
+++ index.js
@@ -1,41 +1,15 @@
 console.log("Bot is starting ...");
 const Discord = require("discord.js");
 const auth = require("./auth.json");
-const EmbedMessage = require("./View/EmbedMessage");
 const CommandsController = require("./Controller/CommandsController");
 const Client = new Discord.Client;
-const prefix = "/";
+const commandPrefix = "/";
 
 Client.on("ready", ()=>{
     console.log("...Bot started !");
 });
 
-Client.on('message', function (evt) {
-    //verifie que celui qui envoie le message est un bot
-    if(evt.author.bot) return;
-    //si le message commence par quelque chose de different que le prefixe
-    if(!evt.content.startsWith(prefix)) return;
-
-    //on supprime le prefixe
-    let commandBody = evt.content.slice(prefix.length);
-    //on divise le message en tableau
-    let args = commandBody.split(" ");
-    //on supprime le premier element du tableau qu'on retourne dans command
-    //et on le converti en lettre minuscule
-    let command = args.shift().toLowerCase();
-
-    switch(command){
-        case "lmt":
-            let test = new EmbedMessage(
-                Client,
-                "Wtf is this bot ?", 
-                "LearnMoreTech is a bot for being aware of the latest news in the field of tech and IT development !"
-            );
-            //evt.reply("Salut moi c'est toto, je viens du ghetto, pour te faire la peau, avec tout mes potos 💣");
-            //evt.channel.send("SALUT");
-            evt.channel.send(test.example);
-            break;
-    }
-});
+let controller = new CommandsController(Client, commandPrefix);
+controller.listen();
 
 Client.login(auth.TOKEN);
